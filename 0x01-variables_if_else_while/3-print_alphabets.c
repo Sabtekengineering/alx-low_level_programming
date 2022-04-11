287cc654430ee4b47c2707efa77eea9d0f19c00f
sabtekengineering <ebukadike92@gmail.com>
2022-04-11 16:04:26 -0700

    work

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,17 +5,24 @@
  * followed by a new line
  * *Return: Always 0 (Success)
  */
+
 int main(void)
+
 {
+
 int ch;
+
 for (ch = 'a';
 ch <= 'z';
 ch++)
 putchar(ch);
+
 for (ch = 'A';
 ch <= 'Z';
 ch++)
 putchar(ch)
 putchar('\n');
+
  return (0);
+
  }
