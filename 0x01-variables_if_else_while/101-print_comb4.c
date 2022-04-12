fd7c3004fe3bd41d3c225e6be3e029f8438fd529
sabtekengineering <ebukadike92@gmail.com>
2022-04-11 17:39:19 -0700

    Trust God

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,32 +13,30 @@ nclude <stdio.h>
  * You are not allowed to use any variables of type `char`.
  * Return: 0
  */
-
 int main(void)
 {
-		int i, j, k, l;
+int i, j, k, l;
+for (i = 0; i < 1000; i++)
+{
+j = i / 100; /* hundreds */
+k = (i / 10) % 10; /* tens */
+l = i % 10; /* singles */
 
-			for (i = 0; i < 1000; i++)
-					{
-								j = i / 100; /* hundreds */
-										k = (i / 10) % 10; /* tens */
-												l = i % 10; /* singles */
+if (j < k && k < l)
+{
+putchar(j + '0');
+putchar(k + '0');
+putchar(l + '0');
 
-														if (j < k && k < l)
-																	{
-																					putchar(j + '0');
-																								putchar(k + '0');
-																											putchar(l + '0');
-
-																														if (i < 700)
-																																		{
-																																							putchar(44);
-																																											putchar(32);
-																																														}
-																																}
-															}
+{
+if (i < 700)
+putchar(44);
+putchar(32);
+}
+}
+}
 putchar('\n');
 
 return (0);
-
 }
+
