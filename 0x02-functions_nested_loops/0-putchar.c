59f6c0e86dff791f7d0c1c5fb7a255d3993a14a7
sabtekengineering <ebukadike92@gmail.com>
2022-04-13 00:26:25 -0700

    My GOD

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * write a program tha prints _putchar, followed by a new line.
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char *ch = "_putchar";
+	while (*ch)
+	{
+		_putchar(*ch);
+		ch++;
+	}
+	_putchar('\n');
+	return (0);
+}
