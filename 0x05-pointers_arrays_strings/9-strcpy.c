e823d22b9d04c128b33c6f0a18b78cd270a5aac6
sabtekengineering <ebukadike92@gmail.com>
2022-04-21 15:11:02 -0700

    My God

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,8 +1,10 @@
 #include "main.h"
+
 /**
  * _strcpy - copy stringat the end of another string including \0.
  * @dest: pointer to string to be copied to.
- * @src: pointer to string to be copied.
+ * *@src: pointer to string to be copied.
+ *
  * Return: pointer to dest.
  */
 char *_strcpy(char *dest, char *src)
@@ -10,7 +12,6 @@ char *_strcpy(char *dest, char *src)
 int j;
 
 for (j = 0; src[j] != '\0'; j++)
-{
 {
 dest[j] = src[j];
 }
