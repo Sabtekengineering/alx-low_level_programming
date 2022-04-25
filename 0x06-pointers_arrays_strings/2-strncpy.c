4c73725da75fedb47c9c4e40c5079a1283236d79
sabtekengineering <ebukadike92@gmail.com>
2022-04-25 14:36:21 -0700

    God is good

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,10 +2,10 @@
 
 /**
  * _strncpy - a function that copies a string
- *  @dest: copy to
- *  @src: copy from
- *  @n: input number of char
- *  Return: Always 0 (Success)
+ * @dest: copy to
+ * @src: copy from
+ * @n: input number of char
+ * Return: Always 0 (Success)
  */
 char *_strncpy(char *dest, char *src, int n)
 {
@@ -14,6 +14,7 @@ int i;
 for (i = 0; src[i] != '\0'; i++)
 if (i < n)
 dest[i] = src[i];
+while (i < n)
 dest[i++] = '\0';
 
 return (dest);
