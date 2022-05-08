dd5537c9f09d7022220f34989dd31ee78c33d7e4
Sabtekengineering <ebukadike92@gmail.com>
2022-05-08 14:16:40 -0700

    trust God

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -1,25 +1,19 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-
 /**
- * free_grid - function to free memory
- * @grid: pointer to grid of int type
- * @height: int type
- * Return: clean up memory
+ * free_grid - allocates a grid, make space and free space
+ * @grid: takes in width of grid
+ * @height: height of grid
+ * Return: free grid
  */
 
 void free_grid(int **grid, int height)
 {
-int y;
+int i;
 
-if (grid == NULL || grid == 0)
+for (i = 0; i < height; i++)
 {
-return;
+free(grid[i]);
 }
-for (y = 0; y < height;  y++)
-{
-free(grid[y]);
-}
+
 free(grid);
 }
