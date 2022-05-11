ac0e6546de624ebf096d4e68c9d4371d9791b87e
Sabtekengineering <ebukadike92@gmail.com>
2022-05-11 11:00:25 -0700

    trust God

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,9 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef DOG_H
+#define DOG_H
 
 /**
- * struct dog - Dog attributes
- * @name: The name of the dog.
- * @age: The age of the dog.
- * @owner: The owner of the dog.
- *
- * Description: The attributes of a dog.
+ * File: dog.h
+ * Desc: Header file that defines a new type struct dog.
  */
 struct dog
 {
@@ -15,14 +11,11 @@ char *name;
 float age;
 char *owner;
 }
-
-/**
- * dog_t - Typedef for dog structure
- */
 typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif
